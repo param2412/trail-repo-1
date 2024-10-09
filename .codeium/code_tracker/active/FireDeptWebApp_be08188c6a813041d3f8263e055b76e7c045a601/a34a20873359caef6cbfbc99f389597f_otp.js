e1056170c2054b89f79a5872c21f62443d05997d
param2412 <paramparikh80@gmail.com>
2024-10-09 10:42:34 +0530

    update

---
+++ .codeium/code_tracker/active/FireDeptWebApp_be08188c6a813041d3f8263e055b76e7c045a601/a34a20873359caef6cbfbc99f389597f_otp.js
@@ -0,0 +1,29 @@
+
+Mc:/Users/lenovo/Desktop/New_React_Project/FireDeptWebApp/backend/utils/otp.js¿import crypto from 'crypto';
+import nodemailer from 'nodemailer';
+import Inspector from '../models/inspector.js';
+
+export function generateOTP() {
+    return crypto.randomInt(100000, 999999).toString(); // 6-digit OTP
+}
+
+export async function sendOTP(email, otp) {
+    let transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: 'param2412parikh@gmail.com',
+            pass: 'your-email-password',
+        },
+    });
+
+    let mailOptions = {
+        from: 'your-email@gmail.com',
+        to: email,
+        subject: 'Your OTP Code',
+        text: `Your OTP code is ${otp}`,
+    };
+
+    await transporter.sendMail(mailOptions);
+}
+… …”
+”¿ "(be08188c6a813041d3f8263e055b76e7c045a601*8c:/Users/lenovo/Desktop/New_React_Project/FireDeptWebApp2Ufile:///c:/Users/lenovo/Desktop/New_React_Project/FireDeptWebApp/backend/utils/otp.js:@file:///c:/Users/lenovo/Desktop/New_React_Project/FireDeptWebApp
